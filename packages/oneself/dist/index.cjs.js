57692bf51619e9fe9d8c8d9f4a15652fbd524054
hazen <hoyeungw@outlook.com>
2022-04-26 12:16:00 +0800

    upgrade package manifest

--- packages/oneself/dist/index.cjs.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', { value: true });
-
-const oneself = x => x;
-const Oneself = () => oneself;
-
-exports.Oneself = Oneself;
-exports.oneself = oneself;
--- packages/oneself/dist/index.esm.js
+++
@@ -1,4 +0,0 @@
-const oneself = x => x;
-const Oneself = () => oneself;
-
-export { Oneself, oneself };
